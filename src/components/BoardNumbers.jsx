51c1fc8c87ba57ef109d76497b7ebbc00c48f337
lucascencig <cenciglucas@gmail.com>
2023-05-10 04:36:49 -0300

    game boardnumbers

--- src/components/BoardNumbers.jsx
+++ src/components/BoardNumbers.jsx
@@ -2,41 +2,51 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import s from './styles/beggining.module.css'
 
-const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-const empty = []
+const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+
 const BoardNumbers = () => {
-  const [selectedNumber, setSelectedNumber] = useState(null)
+  const [selectedNumbers, setSelectedNumbers] = useState([]);
 
   const handleClick = (number) => {
-    setSelectedNumber(number)
-  }
+    if (selectedNumbers.length < 4) {
+      setSelectedNumbers([...selectedNumbers, number]);
+    }
+  };
 
-  useEffect(() => {
-    setSelectedNumber(null)
-  }, [])
+  const handleClear = () => {
+    setSelectedNumbers([]);
+  };
+
+  const correctKey = () => {
+    if (
+      selectedNumbers.length === 4 &&
+      selectedNumbers[0] === '1' &&
+      selectedNumbers[1] === '2' &&
+      selectedNumbers[2] === '3' &&
+      selectedNumbers[3] === '4'
+    ) {
+      alert('¡Código correcto!');
+    }
+  };
 
   return (
     <div className={s.board}>
       <section>
         <div>
-          <input type="text" value={selectedNumber} disabled />
+          <input type="text" value={selectedNumbers.join("")} disabled />
         </div>
       </section>
       <div className={s.game}>
-
-        {
-          numbers.map((e, index) => {
-            return (
-              <button onClick={() => handleClick(index)} key={e} className=''>
-                {index}
-              </button>
-            )
-          }
-          )}
+        {numbers.map((number, index) => (
+          <button onClick={() => handleClick(number)} key={number} className="">
+            {number}
+          </button>
+        ))}
+        <button onClick={handleClear}>Limpiar</button>
+        <button onClick={correctKey}>Probar</button>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default BoardNumbers
+export default BoardNumbers;
